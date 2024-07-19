1100d0b84677e206d7110ebd462b90e699068ac7
Nethmini17 <muthuhewaamnk.20@uom.lk>
2024-07-19 19:18:13 +0530

    implemented the slider component

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,6 +1,23 @@
 import "./slider.scss";
-function Slider() {
-  return <div className="slider">Slider</div>;
+function Slider({images}) {
+  return <div className="slider">
+    <div className="fullSlider">
+        <div className="arrow"><img src="/arrow.png" alt=""/></div>
+        <div className="imageContainer">
+            <img src={images[0]} alt=""  />
+        </div>
+        <div className="arrow"><img src="/arrow.png" className="right" alt=""/></div>
+    </div>
+    <div className="close">X</div>
+    <div className="bigImage">
+        <img src={images[0]} alt="" />
+    </div>
+    <div className="smallImages">
+        {images.slice(1).map((image, index) => (
+            <img key={index} src={image} alt="" />
+        ))}
+    </div>
+  </div>;
 }
 
 export default Slider;
