e101ec52c44646601be145eed4f83dfeafbb9527
Nethmini17 <muthuhewaamnk.20@uom.lk>
2024-07-19 16:09:02 +0530

    implemented the single page

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,6 @@
+import "./slider.scss";
+function Slider() {
+  return <div className="slider">Slider</div>;
+}
+
+export default Slider;
--- src/routes/singlePage/SinglePage.jsx
+++ src/routes/singlePage/SinglePage.jsx
@@ -1,10 +1,42 @@
 import "./singlePage.scss";
+import Slider from "../../components/slider/Slider";
+import { singlePostData } from "../../lib/dummydata";
+import { userData } from "../../lib/dummydata";
+
 function SinglePage() {
-    return ( 
-        <div className="SinglePage">
-Single
+  return (
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="./pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">
+                  <span> $ {singlePostData.price}</span>
+                </div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">
+                {singlePostData.description}
+            </div>
+          </div>
         </div>
-     );
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 }
 
-export default SinglePage ;
+export default SinglePage;
